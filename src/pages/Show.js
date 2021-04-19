a6008fcab31b48e7aefe05b2cbf86ce49b723eff
MDABUSAYED1 <abusayednazim222@gmail.com>
2021-04-20 00:28:24 +0600

    Added elements to the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
+
 import { getAPI } from '../misc/config';
 
 const reducer = (prevState, action) => {
@@ -52,8 +57,6 @@ const Show = () => {
     
   }, [id] ) 
 
-  console.log('show', show);
-
   if(isLoading) {
     return <div>Page is being loaded.</div>
   }
@@ -64,7 +67,31 @@ const Show = () => {
 
   return (
     <div>
-      This is show page
+      <ShowMainData 
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      
+      <h2>Details</h2>
+      <Details 
+        status={show.status}
+        network={show.network}
+        premiered={show.premiered}
+      />
+      
+      <h2>Seasons</h2>
+      <Seasons 
+        seasons={show._embedded.seasons}
+      />
+      
+      <h2>Cast</h2>
+      <Cast 
+        cast={show._embedded.cast}
+      />
+      
     </div>
   )
 }
