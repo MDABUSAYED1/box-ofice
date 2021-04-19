9b59c7a41f053ce8c3143f9336dd169d68a8d785
MDABUSAYED1 <abusayednazim222@gmail.com>
2021-04-19 23:05:41 +0600

    Added API for requested show and mounted the result in show page by checking error

---
+++ src/pages/Show.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getAPI } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [show, setShow ] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect( () => {
+    let isMounted = true; 
+
+    getAPI(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if(isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if(isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      })
+
+      return () => {
+        isMounted = false;
+      } 
+    
+  }, [id] ) 
+
+  console.log('show', show);
+
+  if(isLoading) {
+    return <div>Page is being loaded.</div>
+  }
+
+  if(error) {
+    return <div>Error Occured: {error} </div>
+  }
+
+  return (
+    <div>
+      This is show page
+    </div>
+  )
+}
+
+export default Show
