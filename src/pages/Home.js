58e3698b42b03ebc298eb4e022052f35dc4600ad
MDABUSAYED1 <abusayednazim222@gmail.com>
2021-04-22 01:48:18 +0600

    Created new custom hooks => useLastQuery and useShows

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,10 @@ import { getAPI } from '../misc/config';
 
 import ShowGrid from '../components/show/ShowGrid';
 import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
  
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [radioOption, setRadioOption] = useState('shows');
 
