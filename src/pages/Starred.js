4ccf8c3a018ba5faa8cf1d4bb1c9bc2e46375392
MDABUSAYED1 <abusayednazim222@gmail.com>
2021-04-21 16:59:11 +0600

    Added starred shows into th web page UI

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import ShowGrid from '../components/show/ShowGrid';
+import { getAPI } from '../misc/config';
+import { useShows } from '../misc/custom-hooks'
 
 const Starred = () => {
+  const [starred] = useShows();
+
+  const [isLodaing, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [shows, setShows] = useState(null);
+
+  useEffect( () => {
+    if(starred && starred.length > 1) {
+      const promises = starred.map(showId => getAPI(`/shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        })
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred] );
+
   return (
     <MainPageLayout>
-      This is starred page
+      {isLodaing && <div>Shows is Loading</div>}
+      {error && <div>Error Occured: {error}</div>}
+      {!isLodaing && !shows && <div>No shows were added</div>}
+      {!isLodaing && !error && shows && <div><ShowGrid data={shows} /></div>}
     </MainPageLayout>
   )
 }
